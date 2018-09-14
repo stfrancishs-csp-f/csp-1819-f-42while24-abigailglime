8ad99d74a49d91691d23d79b7a446b970fb9083a
Abigail Glime <20glime@stfrancishs.org>
2018-09-14 01:58:39 +0000

    init

---
+++ loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+   int sum=0;
+   int k;
+
+     k=5;
+
+    while (k>0) {
+        k=k-1; //use something different in "for" version
+        printf ("%d\n", k);
+    }
+
+        for (k=0;k<5;k++)
+    {
+        sum+=k; //use something different in "while" version
+    }
+
+    printf ("%d\n",sum);
+
+}
